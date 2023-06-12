63c694c413e320c81c909dc98ca91b37c55141b8
SaiVenkatesh-Jajula <109030073+SaiVenkatesh-Jajula@users.noreply.github.com>
2023-06-12 08:09:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Hello! This is for deployment in openshift + UPDATED"
+    return "Hello! This is for deployment in openshift + UPDATED + CI/CD using WEBHOOK"
 
 @app.route('/how are you')
 def hello():
